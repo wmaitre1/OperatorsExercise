31b7a24a268afd4e4b09aa90cdb642d73770c611
William Maitre <william.maitre@gmail.com>
2022-07-12 18:57:14 -0400

    Finished project

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            //---------------------part 2 of exercise----------
+
+            var r = 30;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
+                
+            
+
+
+        }
+    }
+}
